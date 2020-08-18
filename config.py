6045cbaae60db1f6cef6700c070d6b7a56a85c13
Arno <arno.deceuninck@student.uantwerpen.be>
2020-08-18 18:44:49 +0200

    Able to post on Facebook

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # An explanation on how to get your page token can be found here:
 # https://www.pythoncircle.com/post/666/automating-facebook-page-posts-using-python-script/
 # Note: under permissions, you have to add "pages_manage_posts" and generate a new token with this
+# Be sure to generate a Page acces token, and not a user access token
 TOKEN = "INSERT_HERE_YOUR_FACEBOOK_PAGE_TOKEN"
 # Page ID can be found at the bottom of the about section of your page
 PAGE_ID = "INSERT_HERE_YOUR_PAGE_ID"
--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from selenium import webdriver
-from token import TOKEN
+from config import TOKEN, PAGE_ID
 import time
 import facebook
+import pickle
 
-confession_nr = 13821  # The first pending confessions
 url = "https://student-confessions.herokuapp.com/confession/{nr}"
+
+# Load the confession nr
+try:
+    confession_nr = pickle.load(open("var.pickle", "rb"))
+except (OSError, IOError) as e:
+    confession_nr = 13821  # The first pending confessions
+    pickle.dump(confession_nr, open("var.pickle", "wb"))
 
 # Selenium configuration
 opts = webdriver.FirefoxOptions()
-opts.headless = False
+opts.headless = True
 driver = webdriver.Firefox(options=opts)
 
 # Load the page
@@ -17,9 +24,26 @@ driver.get(url.format(nr=confession_nr))
 # Get the confession from the page
 element = driver.find_element_by_tag_name('p')  # The confession is the only paragraph on the page
 confession = element.text
-print("Found confession {nr}: {text}".format(nr=confession_nr, text=confession))
 
 # Make a Facebook post
-graph = facebook.GraphAPI(TOKEN)
-facebook_page_id = "67509909999999"
-graph.put_object(facebook_page_id, "feed", message='test message')
+try:
+    graph = facebook.GraphAPI(TOKEN)
+    graph.put_object(PAGE_ID, "feed", message="#{nr} {text}".format(nr=confession_nr, text=confession))
+    confession_nr += 1
+    print("Confession {nr} posted successfully".format(nr=confession_nr))
+
+except facebook.GraphAPIError as error:
+    against_community = "Your content couldn't be shared, because this link goes against our Community Standards"
+    if against_community in error.message:
+        # Feauture idea: Censor the part against the community standards and publish anyway
+        pass
+
+    # Write away the error
+    file_object = open('error.log', 'a+')  # + in case the file doesn't exists
+    file_object.write('{error} [{nr}] {text}\n'.format(nr=confession_nr, text=confession, error=error.code))
+    file_object.close()
+
+    print(error.message)
+
+# Save the confession number and come back later
+pickle.dump(confession_nr, open("var.pickle", "wb"))
